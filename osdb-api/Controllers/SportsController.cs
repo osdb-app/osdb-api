a3550ea486e24441b7b06166880dc5ae8e8d28f9
Reza Teshnizi <rht1369@yahoo.com>
2020-01-05 21:44:16 -0600

    feat(all): Initial commit

---
+++ osdb-api/Controllers/SportsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Runtime;
+
+namespace OsdbApi.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class SportsController : ControllerBase
+	{
+		private const string TABLE_NAME = "osdb";
+		// GET api/sports/getById/1
+		[HttpGet("{id}")]
+		public ActionResult<Model.Sport> GetById(int id)
+		{
+			try
+			{
+				using var client = new AmazonDynamoDBClient(new EnvironmentVariablesAWSCredentials(), Amazon.RegionEndpoint.USEast1);
+				var tables = client.ListTablesAsync().Result;
+				if (!tables.TableNames.Contains(TABLE_NAME))
+				{
+					throw new AmazonDynamoDBException("No suitable table found");
+				}
+				using var dynamoDb = new DynamoDBContext(client);
+				return dynamoDb.LoadAsync<Model.Sport>(1).Result;
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+		}
+	}
+}
---
+++ osdb-api/Model/Sport.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace OsdbApi.Model
+{
+	[DynamoDBTable("osdb")]
+	public class Sport
+	{
+		[DynamoDBHashKey]
+		public int sportId { get; set; }
+		public string name { get; set; }
+		public string organizingBody { get; set; }
+	}
+}
