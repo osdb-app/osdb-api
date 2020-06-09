a69dd1d0856c4630de4ac5048391f20954304c9a
Reza Teshnizi <rht1369@yahoo.com>
2020-06-09 00:20:09 -0500

    chore(launch): Prevent browser at startup

---
+++ osdb-api/Models/Soccer/League.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace OsdbApi.Models.Soccer
+{
+	public class League
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public List<string> Seasons { get; set; }
+	}
+}
